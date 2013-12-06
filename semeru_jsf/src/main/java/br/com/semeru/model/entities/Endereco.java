0c606d98cadad71f2fff84a1fe82bbacc6db5bf7
Alex J. Pereira <aletker@gmail.com>
2013-12-05 22:30:35 -0200

    Adição de atributos na entidade endereço

--- semeru_jsf/src/main/java/br/com/semeru/model/entities/Endereco.java
+++ semeru_jsf/src/main/java/br/com/semeru/model/entities/Endereco.java
@@ -1,15 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.com.semeru.model.entities;
 
-/**
- *
- * @author Alex
- */
-class Endereco {
+import java.io.Serializable;
+import javassist.SerialVersionUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="endereco")
+public class Endereco implements Serializable{
+    private static final long SerialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name="idEndereco", nullable = false)
+    private Integer idEndereco;
+    @Column(name="bairro", length = 80)
+    private String bairro;
+    @Column(name="nomeLogradouro", length = 80)
+    private String nomeLogradouro;    
+    @Column(name="CEP", length = 9)
+    private String cep;    
+    @Column(name="numero")
+    private Integer numero;    
+    @Column(name="complemento", length = 80)
+    private String complemento;
+    
+    
+    
     
 }
